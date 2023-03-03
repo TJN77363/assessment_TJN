89b8c4bd014ce2c63bdd17a25ad7d0d6597c2aa4
Jhen Nee <jneet0915@gmail.com>
2023-03-03 11:13:31 +0800

    Updated MyMethod

---
+++ GrossPay/GrossPay/GrossPay.cs
@@ -0,0 +1,35 @@
+﻿namespace GrossPay
+{
+    public class GrossPay
+    {
+        public const float OverTimeRate = 1.50f;
+        public float BasicPay { get; set; }
+        public int OverTimeHours { get; set; }
+
+        public int CalculateGrossPay()
+        {
+            if (OverTimeHours > 50)
+            {
+                Console.WriteLine("The employee have reached maximum overtime!");
+            }
+            else
+            {
+                Console.WriteLine(BasicPay + (OverTimeHours * OverTimeRate));
+            }
+            return Main();
+        }
+
+        static void Main()
+        {
+            GrossPay Overtime = new GrossPay();  // Create an object of the Car Class (this will call the constructor)
+            Console.WriteLine(Overtime.OverTimeHours);  // Print the value of model
+        }
+
+        //public  int Main()
+        //{
+        //    GrossPay gP = new GrossPay();
+        //    gP.CalculateOverTime();
+        //}
+
+    }
+}
--- Question3/ConsoleApp3/FibonacciSeries.cs
+++ Question3/ConsoleApp3/FibonacciSeries.cs
@@ -1,20 +1,24 @@
-﻿using System;
+﻿
 namespace FibonacciSeries
 {
-    public class Program
+    public class FibonacciSeries
     {
+        static void MyMethod()
+        {
+            int firstNumber = 1, SecondNumber = 1, nextNumber, terms = 20;
+            Console.Write(firstNumber + " " + SecondNumber + " ");
+            for (int i = firstNumber; i < terms; i++)
+            {
+                nextNumber = firstNumber + SecondNumber;
+                Console.Write(nextNumber + " ");
+                firstNumber = SecondNumber;
+                SecondNumber = nextNumber;
+            }
+        }
+
         public static void Main()
         {
-            int firstNumber = 0, SecondNumber = 1, nextNumber, terms;
-            terms = 20;
-
-                for(int i = 2; i < terms; i++)
-                {
-                    nextNumber = firstNumber + SecondNumber;
-                    Console.Write(firstNumber + nextNumber + " ");
-                    firstNumber = SecondNumber;
-                    SecondNumber = nextNumber;
-                }
+            MyMethod();
         }
     }
 }
