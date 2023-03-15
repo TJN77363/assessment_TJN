3aa45b712831dfb3fc9d7ff3877eb04bcaab56ec
Jhen Nee <jneet0915@gmail.com>
2023-03-15 09:14:35 +0800

    TitleCase

---
+++ Question4/TitleCase/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string input = "the lord of the ring";
+        string[] conjunctions = { "and", "the", "a", "of", "for" };
+
+        TextInfo textInfo = CultureInfo.CurrentCulture.TextInfo;
+        string[] words = input.Split(' ');
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (Array.IndexOf(conjunctions, words[i]) == -1)
+            {
+                words[i] = textInfo.ToTitleCase(words[i]);
+            }
+        }
+        string output = string.Join(" ", words);
+        Console.WriteLine(output);
+    }
+}
